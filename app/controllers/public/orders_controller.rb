6f54b5340360875457134906702f9158b5dd9d85
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2022-12-15 16:08:58 +0000

    [19th]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,13 +6,30 @@ class Public::OrdersController < ApplicationController
 
   def new
       @addresses = current_customer.addresses
+      @order = Order.new
+  end
+
+  def confirm
+      @order = Order.new(order_params)
+      @cart_items = current_customer.cart_items
+      @total = 0
+      if params[:address]=="0"
+          @order.shipping_addrress=current_customer.address
+          @order.shipping_postal_code=current_customer.postal_code
+          @order.shipping_name=current_customer.last_name + current_customer.first_name
+      elsif params[:address]=="1"
+      elsif params[:address]=="2"
+          @order.shipping_addrress=params[:shipping_addrress]
+          @order.shipping_postal_code=params[:shipping_postal_code]
+          @order.shipping_name=params[:shipping_name]
+      end
   end
   
-  def confirm
+  def complete
   end
 
   private
   def order_params
-      params.require(:order).permit(:shipping_addrress, :shipping_name, :postage, :billing_amount, :payment_method, :order_status)
+      params.require(:order).permit(:shipping_addrress, :shipping_postal_code, :shipping_name, :postage, :billing_amount, :payment_method, :order_status)
   end
 end
